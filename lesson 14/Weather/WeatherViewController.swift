8fbe299d407c5e92b79b3209342418b8dd7ceeae
Koste-man <kostia.mosk@gmail.com>
2021-03-02 23:05:52 +0300

    many changes in weather

--- lesson 14/Weather/WeatherViewController.swift
+++ lesson 14/Weather/WeatherViewController.swift
@@ -17,7 +17,14 @@ class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet weak var tableView: UITableView!
     
     let realm = try! Realm()
-    var temperature = [SearchResponse]()
+    var temperature = [SearchResponse](){
+        didSet{
+            try! realm.write{
+                realm.add(temperature)
+            }
+        }
+    }
+    
     
     var calendar = Calendar.current
     let today = Date()
@@ -28,21 +35,10 @@ class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         temperature = realm.objects(SearchResponse.self).map({ $0 })
+        loadWeather()
         tableView.delegate = self
         tableView.dataSource = self
     }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        realmSave()
-        print(temperature)
-    }
-    
-    func realmSave(){
-        try! realm.write{
-            realm.add(temperature)
-        }
-    }
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         daysAmount.count
@@ -50,42 +46,43 @@ class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TemperatureTableViewCell
+                //CellTemperatureLabel
         if !temperature.isEmpty{
             cell.temperatureLabel.text = "\(temperature[indexPath.row].temp)"
         }
-        for day in daysAmount{
-            if indexPath.row == day{
-                //CellDateLabel
-                let midnight = calendar.startOfDay(for: today)
-                dateFormat.dateFormat = "dd-MM-YYYY"
-                let formattedDate = dateFormat.string(from: calendar.date(byAdding: .day, value: day, to: midnight)!)
-                cell.dateLabel.text = "\(formattedDate)"
-                
-                //CellWeatherLabel
-                let url = URL(string: "http://api.openweathermap.org/data/2.5/forecast?q=moskva&appid=b65da8724c62e02aa8372c570bdf339d&units=metric")!
-                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                    if let data = data{
-                        do{
-                            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any]{
-                                let list = json["list"] as? [[String:Any]]
-                                let main = list![8*day]["main"] as? [String:Any]
-                                let temp = main?["temp"] as? Double
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 2){
-                                    cell.temperatureLabel.text = "\(temp!) °C"
-                                    let newTemp = SearchResponse()
-                                    newTemp.temp = temp!
-                                    self.temperature.append(newTemp)
-                                }
-                            }
-                        }catch{
-                            print("json error")
-                        }
-                    }
-                }
-                task.resume()
-            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                cell.temperatureLabel.text = "\(self.temperature[indexPath.row].temp)"
         }
+                //CellDateLabel
+        let midnight = calendar.startOfDay(for: today)
+        dateFormat.dateFormat = "dd-MM-YYYY"
+        let formattedDate = dateFormat.string(from: calendar.date(byAdding: .day, value: indexPath.row, to: midnight)!)
+        cell.dateLabel.text = "\(formattedDate)"
+        
         return cell
     }
-    
+    func loadWeather(){
+        for day in daysAmount{
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/forecast?q=moskva&appid=b65da8724c62e02aa8372c570bdf339d&units=metric")!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data{
+                do{
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any]{
+                        let list = json["list"] as? [[String:Any]]
+                        let main = list![8*day]["main"] as? [String:Any]
+                        let temp = main?["temp"] as? Double
+                        DispatchQueue.main.asyncAfter(deadline: .now()){
+                            let newTemp = SearchResponse()
+                            newTemp.temp = temp!
+                            self.temperature.append(newTemp)
+                        }
+                    }
+                }catch{
+                    print("json error")
+                }
+            }
+        }
+        task.resume()
+    }
+    }
 }
