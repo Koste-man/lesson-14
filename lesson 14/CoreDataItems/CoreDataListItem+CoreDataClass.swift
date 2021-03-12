8f629347739dc66ab53526bef73362d10be24ef6
Koste-man <kostia.mosk@gmail.com>
2021-03-12 14:02:59 +0300

    123

---
+++ lesson 14/CoreDataItems/CoreDataListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataListItem+CoreDataClass.swift
+//  lesson 14
+//
+//  Created by Konstantin Moskvichev on 12.03.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreDataListItem)
+public class CoreDataListItem: NSManagedObject {
+
+}
